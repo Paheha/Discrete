f21e3091becd0f24e66c4b0298f63840ca6b2af9
Paheha <1gimbarr1@mail.ru>
2016-12-05 12:24:52 +0300

    Add files via upload

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    int m,n,k,h,g;
+    int m,n,S,T,g;
     ifstream f;
     f.open("C:\\Users\\Gimarr\\Documents\\Tree\\Matrix.txt");
     if (!f) {
@@ -20,22 +20,22 @@ int main(int argc, char *argv[])
     vector <vector <int> > Matrix (n, vector<int> (n,0));
     vector <vector <int> > Vertex(n, vector<int> (n,0));
     for(int i = 0; i < m; i++) {
-        f >> k >> h >> g;
-        if (Matrix [k][h]==0) {
-            Matrix [k][h]=g;
-            Matrix [h][k]=g;
-            Vertex [h][k]=i;
-            Vertex [k][h]=i;
+        f >> S >> T >> g;
+        if (Matrix [S][T]==0) {
+            Matrix [S][T]=g;
+            Matrix [T][S]=g;
+            Vertex [T][S]=i;
+            Vertex [S][T]=i;
             } else
-                if (g < Matrix [k][h]){
-                Matrix [k][h]=g;
-                Matrix [h][k]=g;
-                Vertex [k][h]=i;
-                Vertex [h][k]=i;
+                if (g < Matrix [S][T]){
+                Matrix [S][T]=g;
+                Matrix [T][S]=g;
+                Vertex [S][T]=i;
+                Vertex [T][S]=i;
                 }
     }
 
-    queue <int> Numbers;
+    queue <int> Edges;
     vector <bool> Labels(n, false);
 
     for(int i = 0; i < n; i++){
@@ -50,35 +50,36 @@ int main(int argc, char *argv[])
         cout << endl;
     }
     int min;
-    min=32767;
+    min = 32767;
     bool b;
     while (1) {
         for(int i = 0; i < n;i++)
             for(int g = 0; g < i; g++){
-                cout <<"min egde = "<< min <<"  Matrix["<<i<< "]["<<g<<"] = " << Matrix[i][g] << endl;
-                if(( min >= Matrix[i][g] ) && ( Labels[i]==false || Labels[g]==false  )&& Matrix[i][g]!=0){
-                    min=Matrix[i][g];
-                    k=i;
-                    h=g;
-                    b=true;
+
+                if(( Matrix[i][g] <= min ) && (Labels[i] == false || Labels[g] == false) && (Matrix[i][g]!=0)){
+                    min = Matrix[i][g];
+                    S = i;
+                    T = g;
+                    b = true;
+                    cout <<"min egde = "<< min <<"  Matrix["<<i<< "]["<<g<<"] = " << Matrix[i][g] << endl;
                 }
             }
 
-        cout << endl << "push " <<Vertex[k][h] <<"  k = "<<k <<" h = "<< h <<endl;
-        Matrix[k][h]=32767;
-        Numbers.push(Vertex[k][h]);
-        Labels[k] = true;
-        Labels[h] = true;
+        cout << endl << "push " <<Vertex[S][T] <<"  S = "<<S <<" T = "<< T <<endl;
+        Matrix[S][T] = 32767;
+        Edges.push(Vertex[S][T]);
+        Labels[S] = true;
+        Labels[T] = true;
 
         min = 32767;
         if (!b) break;
         b = false;
     }
-        cout << Numbers.size() <<endl;
+        cout << Edges.size() << endl;
         cout << endl;
-        for(int i = 0; i <= Numbers.size(); i++){
-            cout << Numbers.front() << " ";
-            Numbers.pop();
+        for(int i = 0; i <= Edges.size(); i++){
+            cout << Edges.front() << " ";
+            Edges.pop();
        }
 
     return a.exec();
